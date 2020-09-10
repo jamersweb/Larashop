2958618c4c8931168187d47cd6beed4b253c1db8
AsadChetan11 <asad.chetan11@gmail.com>
2020-09-10 19:19:06 +0500

    user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -47,7 +47,7 @@ class UsersController extends Controller
     public function store()
     {
 
-        /*request()->validate([
+        request()->validate([
             'uname'=> 'required',
             'useremail'=> 'required',
             'fname'=>'required',
@@ -55,7 +55,7 @@ class UsersController extends Controller
             'pasword' => 'required',
             'userroll' => 'required'
 
-        ]);*/
+        ]);
 
         $user = new userprofiles();
 
